2014a19d086139ed0184b50e6fb0e6b35e6f5072
gspvsr <153313380+gspvsr@users.noreply.github.com>
2024-02-11 18:19:05 -0600

    changes in file

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ module "catalogue_instance" {
   vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
   # it should be in roboshop DB subnet
   subnet_id = element(split(",",data.aws_ssm_parameter.private_subnet_ids.value), 0)
-  aws_iam_instance_profile = "catalogue_profile"
+  aws_iam_instance_profile = aws_iam_instance_profile.catalogue_profile.name
   //user_data = file("catalogue.sh")
   #this is optional, if we dont give this will be provisioned inside default subnet of default vpc
   #subnet_id = local.public_subnet_ids[0] # public subnet of default VPC
